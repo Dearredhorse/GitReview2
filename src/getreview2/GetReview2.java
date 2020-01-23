113ff1dda4ba0d51cf77dc8d97bc82f8e5257edc
Dearredhorse <majunx@sheridancollege.ca>
2020-01-23 14:43:49 -0500

    first commit

---
+++ src/getreview2/GetReview2.java
@@ -0,0 +1,17 @@
+
+package getreview2;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Junxiu Ma
+ */
+public class GetReview2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
