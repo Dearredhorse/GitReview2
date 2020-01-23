8032a1ca0f524082ea691f59021685c3dff4b9c2
Junxiu Ma <55039335+Dearredhorse@users.noreply.github.com>
2020-01-23 15:03:35 -0500

    Update GetReview2.java
    from gitbub

--- src/getreview2/GetReview2.java
+++ src/getreview2/GetReview2.java
@@ -12,6 +12,8 @@ public class GetReview2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("modified from the github");
     }
 
 }
